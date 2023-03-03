12bf011f1f1fa7c68fddb4fd4a437e7425dabc74
renatobrasil1 <nato-brasil@hotmail.com>
2023-03-03 13:38:29 -0300

    Criada um objeto SmartTv sobre orientação do prof: Gleyson Sampaio da Dio

---
+++ Operadores/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean variavel= true;
+
+       // System.out.println(!variavel);
+        variavel = ! variavel;
+        System.out.println(variavel);
+    }
+}
